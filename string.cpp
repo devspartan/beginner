aea75998a618e3e40a8cb11c39037654a66ddfcd
Devendra Singh <sdev6631@gmail.com>
2021-03-05 19:47:57 +0530

    2021-03-04

--- string.cpp
+++ string.cpp
@@ -6,11 +6,11 @@ string longestPalindrome(string str)
 {
     int size = str.length();
 
-    if(size == 0) {
+    if (size == 0) {
         return "";
     }
 
-    bool **dp = new bool *[size];
+    bool** dp = new bool* [size];
 
     int maxlen = 1;
     int stIdx = 0;
@@ -28,24 +28,24 @@ string longestPalindrome(string str)
 
     int count = 1;
     for (int i = 0; i < size - 1; i++) {
-        if(str[i] == str[i+1]) {
-            dp[i][i+1] = 1;
+        if (str[i] == str[i + 1]) {
+            dp[i][i + 1] = 1;
             stIdx = i;
             maxlen = 2;
         }
     }
 
-    for(int k = 2; k < size; k++) {
-        for (int i = 0; i < size-k; i++) {
+    for (int k = 2; k < size; k++) {
+        for (int i = 0; i < size - k; i++) {
             int j = i + k;
             dp[i][j] = (str[i] == str[j]) ? true : false;
             if (dp[i][j]) {
                 dp[i][j] = dp[i + 1][j - 1];
             }
 
-            if(dp[i][j]) {
-                int diff = abs(j-i) + 1;
-                if(diff > maxlen) {
+            if (dp[i][j]) {
+                int diff = abs(j - i) + 1;
+                if (diff > maxlen) {
                     maxlen = diff;
                     stIdx = i;
                 }
@@ -65,17 +65,17 @@ string longestPalindrome(string str)
     // }
 
     bool st[size];
-    for(int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
         st[i] = false;
     }
-    int len = size-1;
+    int len = size - 1;
     int countNonOverlappingSub = 0;
-    for(int i = 0; i < size; i++) {
-        for(int j = 0; j <= i; j++) {
-            if(dp[j][j+len] == 1 && !st[j]) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j <= i; j++) {
+            if (dp[j][j + len] == 1 && !st[j]) {
                 countNonOverlappingSub++;
-            cout << "len: " << len+1 << "  st: " << j << " ed: " << j+len << "  " << dp[j][j+len] << endl;
-                for(int k = j; k <= j+len; k++) {
+                cout << "len: " << len + 1 << "  st: " << j << " ed: " << j + len << "  " << dp[j][j + len] << endl;
+                for (int k = j; k <= j + len; k++) {
                     st[k] = true;
                 }
             }
@@ -96,7 +96,7 @@ string longestPalindrome(string str)
     string res;
     if (maxlen > 0)
     {
-        for (int i = stIdx; i < stIdx+maxlen; i++)
+        for (int i = stIdx; i < stIdx + maxlen; i++)
         {
             res.push_back(str[i]);
         }
@@ -111,13 +111,13 @@ void swap(string& str, int i, int j) {
     str[j] = temp;
 }
 
-void reverseString(string& str){
+void reverseString(string& str) {
     int size = str.size();
-    for(int i = 0; i < size/ 2; i++) {
-        swap(str, i, size-i-1);
+    for (int i = 0; i < size / 2; i++) {
+        swap(str, i, size - i - 1);
     }
 
-    for(int i = 0; i< size; i++) {
+    for (int i = 0; i < size; i++) {
         str[i] = (str[i] == ')') ? '(' : ')';
     }
 }
@@ -125,7 +125,7 @@ void reverseString(string& str){
 int longestValidParenthesis(string str) {
     int size = str.size();
 
-    if(size < 1) {
+    if (size < 1) {
         return 0;
     }
 
@@ -133,9 +133,9 @@ int longestValidParenthesis(string str) {
     int close = 0;
     int maxlen = 0;
     int len = 0;
-    for(int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
 
-        if(str[i] == '(') {
+        if (str[i] == '(') {
             open++;
         }
         else {
@@ -144,14 +144,14 @@ int longestValidParenthesis(string str) {
         // cout << "i " << i << "  op: " << open << " " << "clos: " << close << " mxl " << maxlen << endl;
         len++;
 
-        if(close == open) {
-            if(len > maxlen) {
+        if (close == open) {
+            if (len > maxlen) {
                 maxlen = len;
                 open = 0;
                 close = 0;
             }
         }
-        else if(close > open) {
+        else if (close > open) {
             open = 0;
             close = 0;
             len = 0;
@@ -168,13 +168,13 @@ int longestValidParenthesisStack(string str) {
 
     int len = 0;
     int maxlen = 0;
-    for(int i = 0; i < size; i++) {
-        if(str[i] == '(') {
+    for (int i = 0; i < size; i++) {
+        if (str[i] == '(') {
             index.push(i);
             parenthesis.push('(');
         }
         else {
-            if(!parenthesis.empty() && parenthesis.top() == '(') {
+            if (!parenthesis.empty() && parenthesis.top() == '(') {
                 index.pop();
                 parenthesis.pop();
                 len = i - index.top();
@@ -184,7 +184,7 @@ int longestValidParenthesisStack(string str) {
             }
         }
 
-        if(len > maxlen) {
+        if (len > maxlen) {
             maxlen = len;
         }
     }
@@ -195,27 +195,27 @@ int longestValidParenthesisStack(string str) {
 int longestValidPrenthesisDP(string str) {
     int size = str.size();
     cout << "size " << size << endl;
-    if(size < 1) {
+    if (size < 1) {
         return 0;
     }
 
-    int dp[size] = {0};
+    int dp[size] = { 0 };
     int maxlen = 0;
-    for(int i = 1; i < size; i++) {
-        if(str[i] == '(') {
+    for (int i = 1; i < size; i++) {
+        if (str[i] == '(') {
             dp[i] = 0;
         }
-        else if(str[i] == ')') {
-            if(str[i-1] == '(') {
-                dp[i] = dp[i-2] + 2;
+        else if (str[i] == ')') {
+            if (str[i - 1] == '(') {
+                dp[i] = dp[i - 2] + 2;
             }
-            else if(str[i - dp[i - 1] - 1] == '(') {
-                dp[i] = dp[i-1] + 2 + dp[i - dp[i-1] - 2];
+            else if (str[i - dp[i - 1] - 1] == '(') {
+                dp[i] = dp[i - 1] + 2 + dp[i - dp[i - 1] - 2];
             }
         }
     }
-    for(int i = 0; i < size; i++) {
-        if(dp[i] > maxlen) {
+    for (int i = 0; i < size; i++) {
+        if (dp[i] > maxlen) {
             maxlen = dp[i];
         }
         cout << dp[i] << " ";
@@ -236,8 +236,8 @@ bool isInterleave(string s1, string s2, string s3) {
     {
         int oldi = i;
         int oldj = j;
-        if(fst) {
-            if(s1[i] == s3[k]) {
+        if (fst) {
+            if (s1[i] == s3[k]) {
                 i++;
                 k++;
             }
@@ -246,42 +246,42 @@ bool isInterleave(string s1, string s2, string s3) {
                 fst = false;
             }
         }
-        if(scnd) {
-            if(s2[j] == s3[k]) {
+        if (scnd) {
+            if (s2[j] == s3[k]) {
                 j++;
                 k++;
             }
-            else{
+            else {
                 fst = true;
                 scnd = false;
             }
         }
     }
-    
 
-return true;
+
+    return true;
 
 }
 
 bool isInterleaveDPhelper(string s1, string s2, string s3, int i, int j, string res, int** dp) {
-    if(res == s3 && i == s1.length() && j == s2.length()) {
+    if (res == s3 && i == s1.length() && j == s2.length()) {
         return true;
     }
 
-    if(dp[i][j] != -1) {
+    if (dp[i][j] != -1) {
         return (dp[i][j] == 1);
     }
 
     bool ans = false;
-    if(i < s1.length()) {
-        
-        ans = isInterleaveDPhelper(s1, s2, s3, i+1, j, res + s1.at(i), dp) && true;
-        
+    if (i < s1.length()) {
+
+        ans = isInterleaveDPhelper(s1, s2, s3, i + 1, j, res + s1.at(i), dp) && true;
+
     }
-    if(j < s2.length()) {
-        ans = isInterleaveDPhelper(s1, s2, s3, i, j+1, res + s2.at(j), dp) && true;
+    if (j < s2.length()) {
+        ans = isInterleaveDPhelper(s1, s2, s3, i, j + 1, res + s2.at(j), dp) && true;
     }
-    
+
     dp[i][j] = ans == true ? 1 : 0;
     return ans;
 }
@@ -292,20 +292,20 @@ bool isInterleaveDP(string s1, string s2, string s3) {
     int k = s3.length();
     cout << "im herer ";
 
-    if(m + n != k) {
+    if (m + n != k) {
         return false;
     }
-    if((s1.empty() && s2 != s3) || (s2.empty() && s1 != s3)) {
+    if ((s1.empty() && s2 != s3) || (s2.empty() && s1 != s3)) {
         return false;
     }
-    int **dp = new int*[m];
-    for(int i = 0; i < m; i++) {
+    int** dp = new int* [m];
+    for (int i = 0; i < m; i++) {
         dp[i] = new int[n];
-        for(int j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++) {
             dp[i][j] = -1;
         }
     }
-    
+
     string res = "";
     bool ans = isInterleaveDPhelper(s1, s2, s3, 0, 0, res, dp);
 
@@ -318,30 +318,30 @@ int countCharacters(vector<string>& words, string chars) {
     int size = words.size();
     int len = chars.length();
 
-    if(size == 0) {
+    if (size == 0) {
         return 0;
     }
-    else if(len == 0) {
+    else if (len == 0) {
         return 0;
     }
 
     vector<int> hash(26), tempHash;
-    for(int i = 0; i < 26; i++) {
+    for (int i = 0; i < 26; i++) {
         hash[i] = 0;
-    }        
+    }
 
-    for(int i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++) {
         hash[chars[i] - 97]++;
     }
-    
+
     int maxlen = 0;
-    for(int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
         int tempLen = words[i].length();
         bool found = true;
         tempHash = hash;
-        
-        for(int j = 0; j < tempLen; j++) {
-            if(tempHash[words[i][j] - 97] == 0) {
+
+        for (int j = 0; j < tempLen; j++) {
+            if (tempHash[words[i][j] - 97] == 0) {
                 found = false;
                 break;
             }
@@ -350,18 +350,18 @@ int countCharacters(vector<string>& words, string chars) {
             }
         }
 
-        if(found) {
+        if (found) {
             maxlen += tempLen;
         }
-        
+
     }
 
     return maxlen;
 }
 
 int getNextCharIdx(string s, char c, int current) {
-    for(int i = current+1; i < s.length(); i++) {
-        if(s[i] == c) {
+    for (int i = current + 1; i < s.length(); i++) {
+        if (s[i] == c) {
             return i;
         }
     }
@@ -374,22 +374,24 @@ vector<int> shortestToChar(string s, char c) {
     int c1 = -1;
     int c2 = getNextCharIdx(s, c, -1);
 
-    for(int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
         int k1 = abs(c1 - i);
         int k2 = abs(c2 - i);
 
-        if(c1 == -1) {
+        if (c1 == -1) {
             res.push_back(k2);
-        } else if(c2 == -1) {
+        }
+        else if (c2 == -1) {
             res.push_back(k1);
         }
-        else if(k1 < k2) {
+        else if (k1 < k2) {
             res.push_back(k1);
-        } else {
+        }
+        else {
             res.push_back(k2);
         }
 
-        if(k2 == 0) {
+        if (k2 == 0) {
             c1 = c2;
             c2 = getNextCharIdx(s, c, c2);
         }
@@ -403,33 +405,67 @@ vector<int> shortestToChar(string s, char c) {
 }
 
 int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
-    
+
     int searchIdx = 0;
 
-   if(ruleKey == "type") {
-       searchIdx = 0;
-   } else if(ruleKey == "color") {
-       searchIdx = 1;
-   } else if(ruleKey == "name") {
-       searchIdx = 2;
-   }
+    if (ruleKey == "type") {
+        searchIdx = 0;
+    }
+    else if (ruleKey == "color") {
+        searchIdx = 1;
+    }
+    else if (ruleKey == "name") {
+        searchIdx = 2;
+    }
 
     int resCount = 0;
-   for(int i = 0; i < items.size(); i++) {
-        if(items[i][searchIdx] == ruleValue)   {
+    for (int i = 0; i < items.size(); i++) {
+        if (items[i][searchIdx] == ruleValue) {
             resCount++;
-        }   
-   }
-   return resCount;
+        }
+    }
+    return resCount;
 
 }
 
+void letterCasePerUtil(string s, int i, vector<string>& res) {
+
+    if (i == s.length()) {
+        cout << "git " << s << " " << endl;
+        res.push_back(s);
+    }
+    if (i >= s.length()) {
+        cout << "from 1";
+        return;
+    }
+    while (i < s.length() && s[i] > 47 && s[i] < 58) {
+        i++;
+    }
+    
+    letterCasePerUtil(s, i + 1, res);
+
+    s[i] = s[i] - 32;
+    letterCasePerUtil(s, i + 1, res);
+
+    return;
+}
+
+vector<string> letterCasePermutation(string S) {
+    vector<string> res;
+
+    letterCasePerUtil(S, 0, res);
+
+    for (int i = 0; i < res.size(); i++) {
+        cout << res[i] << " ";
+    }
+    return res;
+}
 
 int main()
 {
-    
+
     string str = "fifgbeajcacehiicccfecbfhhgfiiecdcjjffbghdidbhbdbfbfjccgbbdcjheccfbhafehieabbdfeigbiaggchaeghaijfbjhi";
- 
+
     // cout << longestPalindrome(str) << endl;
     // cout << str << endl;
     // reverseString(str)
@@ -438,16 +474,17 @@ int main()
     // cout << longestValidParenthesis(str) << endl;
     // reverseString(str);
     // cout << longestValidParenthesis(str) << endl;
-    vector<vector<string>> items = {{"phone","silver","pixel"},{"computer","silver","lenovo"},{"phone","gold","iphone"}};
-    string ruleKey = "type";
-    string ruleValue = "phone";
-    cout << countMatches(items, ruleKey, ruleValue) << endl;
-    vector<string> vt = {"hello","world","leetcode"};
+    // vector<vector<string>> items = { {"phone","silver","pixel"},{"computer","silver","lenovo"},{"phone","gold","iphone"} };
+    // string ruleKey = "type";
+    // string ruleValue = "phone";
+    // cout << countMatches(items, ruleKey, ruleValue) << endl;
+    vector<string> vt = { "hello","world","leetcode" };
     string parenthesis = "()(((()())(";
-    string cha = "eeeaae";
+    string cha = "a1b4";
+    letterCasePermutation(cha);
     // cout << "parenthieses: " << longestValidParenthesisStack(parenthesis) << endl;
     // cout << countCharacters(vt, cha) << endl;
-    
+
     cout << cha << endl;
     shortestToChar(cha, 'e');
 }
